3e3baed25e76f30b972818a1153b7f33a4854653
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-02-23 21:30:01 +0530

    Completed working on the User Model

--- api/src/models/user.model.ts
+++ api/src/models/user.model.ts
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import config from "config";
 
 export interface UserDocument extends mongoose.Document {
   email: string;
@@ -29,4 +31,23 @@ const userSchema = new mongoose.Schema<UserDocument>(USER, {
   timestamps: true,
 });
 
+userSchema.pre("save", async function (this: UserDocument, next) {
+  const user = this as UserDocument;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"));
+  const hash = bcrypt.hashSync(this.password, salt);
+  this.password = hash;
+  next();
+});
 
+userSchema.methods.comparePassword(async function (
+  this: UserDocument,
+  password: string
+): Promise<boolean> {
+  return bcrypt.compare(password, this.password).catch(() => false);
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
