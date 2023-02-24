41c25fee62c521e47ee803b0c352367e76dbae69
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-02-24 12:04:23 +0530

    Created mongoose post model

---
+++ api/src/models/post.model.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import { UserDocument } from "./user.model";
+
+export interface PostDocument extends mongoose.Document {
+  author: UserDocument["_id"];
+  title: string;
+  summary: string;
+  content: string;
+  cover: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const POST = {
+  title: {
+    type: String,
+    require: true,
+  },
+  summary: {
+    type: String,
+    require: true,
+  },
+  content: {
+    type: String,
+    require: true,
+  },
+  cover: {
+    type: String,
+    require: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+};
+
+const postSchema = new mongoose.Schema<PostDocument>(POST, {
+  timestamps: true,
+});
+
+const postModel = mongoose.model("Post", postSchema);
+
+export default postModel;
---
+++ api/src/services/post.service.ts
@@ -0,0 +1,4 @@
+import mongoose from "mongoose";
+import postModel, { PostDocument } from "../models/post.model";
+
+
