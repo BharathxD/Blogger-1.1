f1622cc7936aede9bcd285718d40f82d6c015fa8
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-02-25 13:32:47 +0530

    Splitted the Posts components for better reusability

--- client/src/pages/posts/Posts.tsx
+++ client/src/pages/posts/Posts.tsx
@@ -1,78 +1,22 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import classes from "./Posts.module.css";
-import { useSelector } from "react-redux";
-
+import Post from "./components/Post";
 
 interface Props {
   isLoggedIn: boolean;
 }
 
-
-const Posts: React.FC<Props> = ({isLoggedIn}) => {
+const Posts: React.FC<Props> = ({ isLoggedIn }) => {
   const navigate = useNavigate();
   useEffect(() => {
     if (isLoggedIn === false) navigate("/");
   }, [isLoggedIn]);
   return (
     <main className={classes.entries}>
-      <div className={classes.entry}>
-        <div className={classes.picture}>
-          <img
-            alt="image"
-            src="https://techcrunch.com/wp-content/uploads/2022/10/CMC_3800.jpg?w=430&h=230&crop=1"
-          />
-        </div>
-        <div className={classes.details}>
-          <h2>Security researchers warn of a new class of Apple bugs</h2>
-          <p>
-            Security researchers say they have uncovered a “new class” of
-            vulnerabilities that could allow attackers to bypass Apple’s
-            security protections in iOS and macOS to access users’ sensitive
-            data. Trellix’s Advanced Research Center published details this week
-            of the privilege escalation vulnerabilities — meaning they allow
-            someone to gain an elevated...
-          </p>
-        </div>
-      </div>
-      <div className={classes.entry}>
-        <div className={classes.picture}>
-          <img
-            alt="image"
-            src="https://techcrunch.com/wp-content/uploads/2022/10/CMC_3800.jpg?w=430&h=230&crop=1"
-          />
-        </div>
-        <div className={classes.details}>
-          <h2>Security researchers warn of a new class of Apple bugs</h2>
-          <p>
-            Security researchers say they have uncovered a “new class” of
-            vulnerabilities that could allow attackers to bypass Apple’s
-            security protections in iOS and macOS to access users’ sensitive
-            data. Trellix’s Advanced Research Center published details this week
-            of the privilege escalation vulnerabilities — meaning they allow
-            someone to gain an elevated...
-          </p>
-        </div>
-      </div>
-      <div className={classes.entry}>
-        <div className={classes.picture}>
-          <img
-            alt="image"
-            src="https://techcrunch.com/wp-content/uploads/2022/10/CMC_3800.jpg?w=430&h=230&crop=1"
-          />
-        </div>
-        <div className={classes.details}>
-          <h2>Security researchers warn of a new class of Apple bugs</h2>
-          <p>
-            Security researchers say they have uncovered a “new class” of
-            vulnerabilities that could allow attackers to bypass Apple’s
-            security protections in iOS and macOS to access users’ sensitive
-            data. Trellix’s Advanced Research Center published details this week
-            of the privilege escalation vulnerabilities — meaning they allow
-            someone to gain an elevated...
-          </p>
-        </div>
-      </div>
+        <Post />
+        <Post />
+        <Post />
     </main>
   );
 };
---
+++ client/src/pages/posts/components/Post.tsx
@@ -0,0 +1,27 @@
+import classes from "./Post.module.css";
+
+const Post: React.FC = () => {
+  return (
+    <div className={classes.entry}>
+      <div className={classes.picture}>
+        <img
+          alt="image"
+          src="https://techcrunch.com/wp-content/uploads/2022/10/CMC_3800.jpg?w=430&h=230&crop=1"
+        />
+      </div>
+      <div className={classes.details}>
+        <h2>Security researchers warn of a new class of Apple bugs</h2>
+        <p>
+          Security researchers say they have uncovered a “new class” of
+          vulnerabilities that could allow attackers to bypass Apple’s security
+          protections in iOS and macOS to access users’ sensitive data.
+          Trellix’s Advanced Research Center published details this week of the
+          privilege escalation vulnerabilities — meaning they allow someone to
+          gain an elevated...
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
