db1905904394b7c014e2f47f57d1c38722901bb3
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-02-25 13:20:04 +0530

    Made the route api/post that will take a post request to store the post details and files

--- api/src/controllers/post.controller.ts
+++ api/src/controllers/post.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import fs from "fs";
+import { uploadPost } from "../services/post.service";
+import { verifyJWT } from "../utils/jwt.utils";
 
-export const postHandler = (req: Request, res: Response) => {
+export const postHandler = async (req: Request, res: Response) => {
   if (req.file && req.file.originalname) {
     const { originalname, path } = req.file;
     const parts = originalname.split(".");
@@ -9,6 +11,26 @@ export const postHandler = (req: Request, res: Response) => {
     res.json({ extension: extension });
     const newPath = path + "." + extension;
     fs.renameSync(path, newPath);
+    const { token } = req.cookies;
+    const user = await verifyJWT(token);
+    const author = (user.decoded as { _id: string })._id;
+    const {
+      title,
+      summary,
+      content,
+    }: {
+      title: string;
+      summary: string;
+      content: string;
+    } = req.body;
+    const post = await uploadPost({
+      author: author,
+      title: title,
+      summary: summary,
+      content: content,
+      cover: newPath,
+    });
+  } else {
+    res.send(409);
   }
-  res.status(200).json({ file: req.files });
 };
--- api/src/services/post.service.ts
+++ api/src/services/post.service.ts
@@ -1,2 +1,15 @@
 import mongoose from "mongoose";
 import postModel, { PostDocument } from "../models/post.model";
+
+export const uploadPost = async (
+  input: mongoose.DocumentDefinition<
+    Omit<PostDocument, "createdAt" | "updatedAt">
+  >
+) => {
+  try {
+    const post = await postModel.create(input);
+    return post;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
