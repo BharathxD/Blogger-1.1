afc54ebb9419f38a6ddf64bc4392a889c7c84a58
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-23 09:12:15 +0530

    Refactored the code better readability

--- client/src/pages/posts/components/Post.tsx
+++ client/src/pages/posts/components/Post.tsx
@@ -4,22 +4,22 @@ import { Link } from "react-router-dom";
 import UserAvatar from "../../../components/layout/Nav/User/UserAvatar";
 import { postsData } from "../../../types/Post.types";
 
-interface Props {
-  post: postsData;
-}
+const Post = ({ post }: { post: postsData }) => {
+  
+  const uri = `http://localhost:3000/${post.cover.replace("src/", "")}`;
 
-const Post: React.FC<Props> = ({ post }) => {
+  const summary = post.summary.split(" ").splice(0, 60).join(" ");
+
+  const createdDate = format(new Date(post.createdAt), "MMM d, yyyy HH:mm");
+
   return (
     <div className={classes.entry}>
       <div className={classes.picture}>
-        <img
-          src={`http://localhost:3000/${post.cover.replace("src/", "")}`}
-          className={classes.image}
-        />
+        <img src={uri} className={classes.image} />
       </div>
       <div className={classes.details}>
         <div className={classes["title-box"]}>
-          <Link className={classes.cta} to={"/posts/" + post._id}>
+          <Link className={classes.cta} to={`/posts/${post._id}`}>
             <span className={classes["hover-underline-animation"]}>
               {post.title}
             </span>
@@ -30,13 +30,12 @@ const Post: React.FC<Props> = ({ post }) => {
                 <UserAvatar url={post.authorProfile} />
               </span>
               <span>
-                {post.author.name} at{" "}
-                {format(new Date(post.createdAt), "MMM d, yyyy HH:mm")}
+                {post.author.name} at {createdDate}
               </span>
             </div>
           </div>
         </div>
-        <p>{post.summary.split(" ").splice(0, 60).join(" ")}</p>
+        <p>{summary}</p>
       </div>
     </div>
   );
